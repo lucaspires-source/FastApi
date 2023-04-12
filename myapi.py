adb2c92154056a2a2fe0cf87bc04d9602ae52f88
lucaspires-source <pires.lucas94@gmail.com>
2023-04-12 10:13:24 -0300

    Learning Query and Path  Parameters

--- myapi.py
+++ myapi.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Path
+from typing import Optional
 
 app = FastAPI()
 
@@ -22,5 +22,13 @@ def index():
 
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description="The id of the student you want to view",gt=0, lt=3)):
+def get_student(student_id: int = Path(..., description="The id of the student you want to view",gt=0, lt=3)):
     return students[student_id]
+
+
+@app.get("/get-by-name")
+def get_student(*, name:Optional[str] = None, test : int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data":"Not Found"}    
